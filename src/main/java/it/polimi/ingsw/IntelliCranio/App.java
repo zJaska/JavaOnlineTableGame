fc9905588f5ee9b25e5e880485ecf3f505c73c6e
mattiteo24 <42670685+mattiteo24@users.noreply.github.com>
2021-03-24 17:50:23 +0100

    Revert "Prova moduli javafx"
    This reverts commit 9038a1de0f1d1632977d75b0d99f064bdadfe7fa.

--- src/main/java/it/polimi/ingsw/IntelliCranio/App.java
+++ src/main/java/it/polimi/ingsw/IntelliCranio/App.java
@@ -4,12 +4,10 @@ package it.polimi.ingsw.IntelliCranio;
  * Hello world!
  *
  */
-public class App {
-
+public class App 
+{
     public static void main( String[] args )
     {
-
         System.out.println( "Hello World!" );
     }
-
 }
