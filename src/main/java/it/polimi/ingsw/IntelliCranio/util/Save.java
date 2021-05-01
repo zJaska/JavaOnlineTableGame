538723caa36ec9340d307c819d046b7f68f10166
matteo-blasoni <matteo.blasoni@mail.polimi.it>
2021-05-01 05:16:04 +0200

    Implemented game database and data clear

--- src/main/java/it/polimi/ingsw/IntelliCranio/util/Save.java
+++ src/main/java/it/polimi/ingsw/IntelliCranio/util/Save.java
@@ -1,13 +1,20 @@
 package it.polimi.ingsw.IntelliCranio.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.IntelliCranio.models.Game;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Save {
 
     private static final String pathPrefix = "src/main/resources/saved_games/";
+    private static final String databasePath = "src/main/resources/database.json";
 
     /**
      * Serialize the game to file using unique IDs.
@@ -45,7 +52,7 @@ public class Save {
      * @param gameUuid Unique ID of the game to load
      * @return A new instance of Game, the deserialized object from file
      */
-    public static Game loadSave(UUID gameUuid) {
+    public static Game loadGame(UUID gameUuid) {
 
         //Path from game unique ID
         String path = pathPrefix + gameUuid.toString();
@@ -69,6 +76,73 @@ public class Save {
         }
 
         return null;
+
+    }
+
+    /**
+     * Get the database of all players in different games.
+     *
+     * @return The hasmap string-uuid of all the nicknames connected to a specific game
+     */
+    public static HashMap<String, UUID> getDatabase() {
+
+        Gson gson = new Gson();
+        String fileData = "";
+
+        try {
+            fileData = new String(Files.readAllBytes(Paths.get(databasePath)));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return gson.fromJson(fileData, new TypeToken<HashMap<String, UUID>>(){}.getType());
+    }
+
+    /**
+     * Save the updated database in its file.
+     * @param database The hashmap to store in a file.
+     */
+    public static void saveDatabase(HashMap<String, UUID> database) {
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try {
+            FileWriter out = new FileWriter(databasePath);
+
+            out.write(gson.toJson(database));
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Remove the file of the game from disk.
+     * Remove every player in database provided its game uuid value
+     * <p>
+     *     WARNING! Deleted data can't be restored.
+     * </p>
+     *
+     * @param uuid The uuid to remove
+     */
+    public static void deleteGameData(UUID uuid) {
+
+        //Delete File from disk
+        //Path from game unique ID
+        String path = pathPrefix + uuid.toString();
+        File file = new File(path);
+        file.delete();
+
+        //Remove from database and save the updated one
+        HashMap<String, UUID> database = getDatabase();
+        database.entrySet().removeIf(e -> e.getValue().equals(uuid));
+        saveDatabase(database);
+
 
     }
 
