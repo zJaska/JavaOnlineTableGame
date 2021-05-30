6fe21b1aad2a0e4734ae786395c643c3c9f00dfe
matteo-blasoni <matteo.blasoni@mail.polimi.it>
2021-05-30 17:44:52 +0200

    Fix Checks

--- src/main/java/it/polimi/ingsw/IntelliCranio/util/Checks.java
+++ src/main/java/it/polimi/ingsw/IntelliCranio/util/Checks.java
@@ -678,8 +678,13 @@ public class Checks {
         ArrayList<T> unifiedActual = Lists.unifyResourceAmounts(actual);
         ArrayList<T> unifiedExpected = Lists.unifyResourceAmounts(expected);
 
+        unifiedActual.forEach(ac -> {
+            if(unifiedExpected.stream().noneMatch(ex -> ex.getType() == ac.getType()))
+                error.set(true);
+        });
+
         unifiedExpected.forEach(ex -> {
-            if (unifiedActual.stream().anyMatch(ac -> ac.getType() != ex.getType()))
+            if (unifiedActual.stream().noneMatch(ac -> ac.getType() == ex.getType()))
                 error.set(true);
             if (unifiedActual.stream().anyMatch(ac -> (ac.getType() == ex.getType() && ac.getAmount() != ex.getAmount())))
                 error.set(true);
